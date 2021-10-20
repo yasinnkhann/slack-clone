a1123578a6f1cb9117a67c5cdb7b39c75f4041e9
Yasin Khan <yasinkhan500@gmail.com>
2021-10-20 16:24:25 -0700

    prepped for deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,80 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebaseConfig.js';
+import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
-  return <div className='app'>hi</div>;
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png'
+            alt=''
+          />
+          <Spinner name='ball-spin-fade-loader' color='purple' fadeIn='none' />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
+
+  return (
+    <div className='app'>
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route exact path='/'>
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    object-fit: contain;
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
