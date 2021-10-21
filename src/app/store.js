590e224f37250b0aaa4929c565250f62e67f9369
Yasin Khan <yasinkhan500@gmail.com>
2021-10-20 20:23:47 -0700

    deployed!

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appReducer from "../features/appSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from '../features/appSlice.js';
 
 export default configureStore({
   reducer: {
